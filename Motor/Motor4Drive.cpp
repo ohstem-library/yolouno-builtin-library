db9473e948cd699f16c73121680c463651d0030d
quang3103 <trannhutquang.bi@gmail.com>
2023-04-30 09:28:29 +0700

    add step motor

--- Motor/Motor4Drive.cpp
+++ Motor/Motor4Drive.cpp
@@ -6,6 +6,11 @@ Motor4Drive::Motor4Drive(uint8_t address){
     _address = address;
 }
 
+Motor4Drive::Motor4Drive(uint8_t address, uint8_t motor_type = DC_MOTOR){
+    _address = address;
+    _motor_type = motor_type;
+}
+
 Motor4Drive::~Motor4Drive(){
     
 }
@@ -18,20 +23,41 @@ bool Motor4Drive::set_motor(int motor_index,int speed){
         }
         return false;
     }
-    if(speed < 0 || speed > 100){
-        if(Serial){
-            Serial.println("Speed index out of range");
+    //DC MOTOR
+    if (_motor_type == DC_MOTOR) { 
+        if(speed < -100 || speed > 100){
+            if(Serial) {
+                Serial.println("Speed index out of range");
+            }   
+            return false;
         }
-        return false;
+        Wire.beginTransmission(_address);
+        uint8_t data_write[4];
+        data_write[0] = DC_MOTOR;
+        data_write[1] = motor_index;
+        data_write[2] = speed > 0 ? 1 : 0;
+        data_write[3] = speed > 0 ? (uint8_t)speed : (uint8_t)(speed * -1);
+        Wire.write(data_write, sizeof(data_write));
+        Wire.endTransmission(_address);
+        return true;
     }
-    Wire.beginTransmission(_address);
-    uint8_t data_write[3];
-    data_write[0] = motor_index;
-    data_write[1] = speed > 0 ? 1 : 0;
-    data_write[2] = speed > 0 ? (uint8_t)speed : (uint8_t)(speed * -1);
-    Wire.write(data_write, sizeof(data_write));
-    Wire.endTransmission(_address);
-    return true;
+    else if (_motor_type == STEP_MOTOR) {     //STEP MOTOR
+        if(speed < -128 || speed > 127){
+            if(Serial) {
+                Serial.println("Speed index out of range");
+            }   
+            return false;
+        }
+        Wire.beginTransmission(_address);
+        uint8_t data_write[4];
+        data_write[0] = STEP_MOTOR;
+        data_write[1] = motor_index;
+        data_write[2] = speed > 0 ? 1 : 0;
+        data_write[3] = (uint8_t)(speed + 128);
+        Wire.write(data_write, sizeof(data_write));
+        Wire.endTransmission(_address);
+        return true;
+    }
 }
 
 bool Motor4Drive::set_motors(int speed){
